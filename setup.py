f45e13ccf8f24690a7f7bc2b7994866ad8c1f5fc
LeonardoReis777 <122556226+LeonardoReis777@users.noreply.github.com>
2024-03-20 17:47:31 -0300

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,18 @@
+import sys
+from cx_Freeze import setup, Executable
+
+# Arquivo principal Python que deseja converter em executável
+main_script = 'oxossibackup.py'
+
+# Configuração do executável
+executables = [Executable(main_script)]
+
+# Configurações adicionais do setup
+build_exe_options = {'packages': [], 'excludes': []}
+
+# Configuração do setup
+setup(name='NomeDoSeuExecutavel',
+      version='1.0',
+      description='Descrição do seu executável',
+      options={'build_exe': build_exe_options},
+      executables=executables)
